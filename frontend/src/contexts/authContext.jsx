e9e29b214523a19419cde76a55f23321c575d419
Chirag Jindal <chirag.jindal.sde@gmail.com>
2025-12-29 19:43:58 +0530

    Update authContext.jsx

--- frontend/src/contexts/authContext.jsx
+++ frontend/src/contexts/authContext.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext(null);
 
 const client = axios.create({
-  baseURL: "http://localhost:8080/api/v1/users",
+  baseURL: "https://video-meet-project.onrender.com/api/v1/users",
 });
 
 export const AuthProvider = ({ children }) => {
